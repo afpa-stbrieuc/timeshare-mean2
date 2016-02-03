1f0b9140f9517cd01b790f1cab60841a63c0857e
sysn00ps <nada.vujanic@gmail.com>
2016-02-03 18:32:49 +0100

    Adding bootstrap to the home page and compte_rendu english and french version

---
+++ app/public/scripts/controllers/mainCtrl.js
@@ -0,0 +1,9 @@
+ 'use strict';
+
+ angular.module('timeShareApp')
+      .controller('MainCtrl', ['$route', '$routeParams', '$location',
+      	function($route, $routeParams, $location) {
+      		this.$route = $route;
+      		this.$location = $location; 
+      		this.$routeParams = $routeParams;
+      	}]);
