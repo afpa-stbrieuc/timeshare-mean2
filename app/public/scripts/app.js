e28180b19b551a1c5f6550e5fd2816e371b24cb1
sysn00ps <nada.vujanic@gmail.com>
2016-02-08 17:27:25 +0100

    <Clearing up mistakes from roll back files>

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 //register all modules
-angular.module('mytodoApp', [
+angular.module('timeShareApp', [
   'ngRoute',
   'ngCookies',
   'ngResource',
@@ -26,4 +26,4 @@ angular.module('mytodoApp', [
       DIALOGS_OK: 'OK'
     });
 
-  }],  ['ngAudio']);
+  }]);
