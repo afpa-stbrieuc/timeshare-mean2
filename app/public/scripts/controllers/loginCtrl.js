cdb5e2adce4d7b2ca3903eab3f53149364f9e408
K4M1coder <kamicth@gmail.com>
2016-02-15 17:02:57 +0100

    WIP implement of passport for user auth

--- app/public/scripts/controllers/loginCtrl.js
+++ app/public/scripts/controllers/loginCtrl.js
@@ -2,11 +2,57 @@
  * Created by cthed on 09/02/2016.
  */
 'use strict';
-angular.module('timeShareApp')
-    .controller('LoginCtrl', ['$route', '$routeParams', '$location',
-        function($route, $routeParams, $location) {
-            this.$route = $route;
-            this.$location = $location;
-            this.$routeParams = $routeParams;
-          }
-    ]);
+//angular.module('timeShareApp')
+//    .controller('LoginCtrl', ['$route', '$routeParams', '$location',
+//        function($route, $routeParams, $location) {
+//            this.$route = $route;
+//            this.$location = $location;
+//            this.$routeParams = $routeParams;
+//          }
+//    ]);
+
+(function() {
+  angular
+  .module('timeShareApp')
+  .controller('loginCtrl', loginCtrl);
+  loginCtrl.$inject = ['$location', 'authentication'];
+
+  function loginCtrl($location, authentication) {
+    var vm = this;
+
+    vm.pageHeader = {
+      title: 'Connexion'
+    }
+
+    vm.credentials = {
+      mail: "",
+      password: ""
+    };
+
+    vm.returnPage = $location.search().page || '/';
+
+    vm.onSubmit = function() {
+      vm.formError = "";
+      if (!vm.credentials.mail || !vm.credentials.password) {
+        vm.formError = "tous les champs sont requis";
+        return false;
+      } else {
+        vm.doLogin();
+      }
+    };
+
+    vm.doLogin = function() {
+      vm.formError = "";
+      authentication
+      .login(vm.credentials)
+      .error(function(err) {
+        vm.formError = err;
+      })
+      .then(function() {
+        $location.search('page', null);
+        $location.path(vm.returnPage);
+      });
+    };
+
+  }
+})();
