1af06b35a58e4c9c945aa825290e4b2661c282dc
K4M1coder <kamicth@gmail.com>
2016-02-16 10:38:27 +0100

    fix console err LoginCtrl (service authenticate was missing) add in nav login form start directive for form

---
+++ app/public/directives/loginFrom.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cthed on 16/02/2016.
+ */
+angular.module('timeShareApp')
+.directive('loginForm', function() {
+  return {
+    templateUrl: 'views/login.html'
+  };
+});
--- app/public/scripts/controllers/loginCtrl.js
+++ app/public/scripts/controllers/loginCtrl.js
@@ -2,55 +2,47 @@
  * Created by cthed on 09/02/2016.
  */
 'use strict';
-//angular.module('timeShareApp')
-//    .controller('LoginCtrl', ['$route', '$routeParams', '$location',
-//        function($route, $routeParams, $location) {
-//            this.$route = $route;
-//            this.$location = $location;
-//            this.$routeParams = $routeParams;
-//          }
-//    ]);
 
 (function() {
   angular
   .module('timeShareApp')
-  .controller('loginCtrl', loginCtrl);
+  .controller('LoginCtrl', loginCtrl);
   loginCtrl.$inject = ['$location', 'authentication'];
 
   function loginCtrl($location, authentication) {
-    var vm = this;
+    var login = this;
 
-    vm.pageHeader = {
+    login.pageHeader = {
       title: 'Connexion'
     }
 
-    vm.credentials = {
+    login.credentials = {
       mail: "",
       password: ""
     };
 
-    vm.returnPage = $location.search().page || '/';
+    login.returnPage = $location.search().page || '/';
 
-    vm.onSubmit = function() {
-      vm.formError = "";
-      if (!vm.credentials.mail || !vm.credentials.password) {
-        vm.formError = "tous les champs sont requis";
+    login.onSubmit = function() {
+      login.formError = "";
+      if (!login.credentials.mail || !login.credentials.password) {
+        login.formError = "tous les champs sont requis";
         return false;
       } else {
-        vm.doLogin();
+        login.doLogin();
       }
     };
 
-    vm.doLogin = function() {
-      vm.formError = "";
+    login.doLogin = function() {
+      login.formError = "";
       authentication
-      .login(vm.credentials)
+      .login(login.credentials)
       .error(function(err) {
-        vm.formError = err;
+        login.formError = err;
       })
       .then(function() {
         $location.search('page', null);
-        $location.path(vm.returnPage);
+        $location.path(login.returnPage);
       });
     };
 
